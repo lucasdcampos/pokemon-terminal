cc9f9e7683f4c8ea5b0b84606c098a51fb1622de
Lucas M. Campos <lucas@lucasof.com>
2024-10-16 21:57:42 -0300

    Add Pokemon class

---
+++ src/Pokemon.java
@@ -0,0 +1,10 @@
+public class Pokemon
+{
+    PokemonData data;
+
+    public Pokemon(PokemonData data) {
+        this.data = data;
+    }
+
+    
+}
---
+++ src/PokemonData.java
@@ -0,0 +1,10 @@
+public class PokemonData
+{
+    String name;
+    int level;
+    int health;
+    int baseAttack;
+    int defense;
+    int speed;
+    int specialAttack;
+}
