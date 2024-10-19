60673172d7e8340ee6425d50d803d95133de398c
Heysatirohere <satirog719@gmail.com>
2024-10-18 22:34:14 -0300

    create inventory

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Inventory {
+
+  private ArrayList<Item> items;
+
+  public Inventory(){
+      this.items = new ArrayList<>();
+  }
+
+  public void addItem(String name, int quantity, int cost) {
+      for (Item item : items) {
+          if(item.getName().equals(name)) {
+              item.setQuantity(item.getQuantity() + quantity);
+          }
+          return;
+      }
+      items.add(new Item(name, quantity, cost));
+  }
+
+  public void removeItem(String name, int quantity) {
+      for (Item item : items) {
+          if (item.getName().equals(name)){
+              int newQuantity = item.getQuantity() - quantity;
+              if (newQuantity <= 0) {
+               items.remove(item);
+              }
+              else {
+                  item.setQuantity(newQuantity);
+              }
+              return;
+          }
+      }
+  }
+
+  public void showInventory() {
+      if (items.isEmpty()) {
+          System.out.println("Tem nada aí");
+      }
+      else {
+          System.out.println("Tu tem isso aí: ");
+          for (Item item : items) {
+              System.out.println(items);
+          }
+      }
+  }
+
 }
--- src/Shop.java
+++ src/Shop.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Shop {
+
+    static int pokebola = 3;
+
+    static void buy() {
+
+
+    }
+
+    static void sell(){
+
+        if (pokebola == 0) {
+            System.out.println("Você pode vender");
+        }
+        else {
+            System.out.println("Você não tem Pokebola para vender");
+        }
+    }
 }
